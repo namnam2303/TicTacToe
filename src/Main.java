1493487cbf24ad9d17ad54708f15f93524031828
namnam2303 <85600809+namnam2303@users.noreply.github.com>
2021-12-11 13:36:31 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
     private static List<List> conditions = new ArrayList<List>();
     private static List<Integer> playerPositions = new ArrayList<>();
     private static List<Integer> cpuPositions = new ArrayList<>();
+
     public static void setConditions(List<List> conditions) {
         conditions.add(Arrays.asList(1, 2, 3));
         conditions.add(Arrays.asList(4, 5, 6));
@@ -20,6 +21,7 @@ public class Main {
     }
 
     private static Scanner _sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         String gameBoard[][] = {{"   ", " | ", "   ", " | ", "   "},
                 {"---", "-+-", "---", "-+-", "---"},
@@ -31,7 +33,7 @@ public class Main {
         while (result == -1) {
             updateGameBoard(gameBoard, getPlayerPos(), "player");
             result = status(gameBoard);
-            if(result != -1) {
+            if (result != -1) {
                 printGameBoard(gameBoard);
                 break;
             }
@@ -39,22 +41,23 @@ public class Main {
             printGameBoard(gameBoard);
             result = status(gameBoard);
         }
-        System.out.println(result == 1 ? "Congratulation! You win" : result == 0 ? "Failed! Cpu wins!" : "Drawn!");
+        System.out.println(result == 1 ? "Congratulations! You win" : result == 0 ? "Failed! Cpu wins!" : "Drawn!");
     }
+
     private static int randomCpuPos() {
         Random random = new Random();
         int cpuPos = 0;
-        if(cpuPositions.size() >=2 ) {
+        if (cpuPositions.size() >= 2) {
             for (List condition : conditions) {
                 int count = 0;
                 for (int i = 0; i < condition.size(); i++) {
-                    if(cpuPositions.contains(condition.get(i))) {
+                    if (cpuPositions.contains(condition.get(i))) {
                         count++;
                     }
                 }
                 if (count == 2) {
                     for (int i = 0; i < condition.size(); i++) {
-                        if(!cpuPositions.contains(condition.get(i))) {
+                        if (!cpuPositions.contains(condition.get(i))) {
                             cpuPos = checkPos((int) condition.get(i)) ? 0 : (int) condition.get(i);
                         }
                     }
@@ -73,29 +76,42 @@ public class Main {
         cpuPositions.add(cpuPos);
         return cpuPos;
     }
+
     private static boolean checkPos(int pos) {
         return playerPositions.contains(pos) || cpuPositions.contains(pos);
     }
+
     private static int status(String[][] gameBoard) {
-        for (List c: conditions) {
-            if(playerPositions.containsAll(c)) {
-                return 1;
-            } else if(cpuPositions.containsAll(c)) {
-                return 0;
-            } else if(playerPositions.size() + cpuPositions.size() == 9) {
-                return 2;
+        if (playerPositions.size() + cpuPositions.size() == 9) {
+            for (List c : conditions) {
+                if (playerPositions.containsAll(c)) {
+                    return 1;
+                } else if (cpuPositions.containsAll(c)) {
+                    return 0;
+                }
             }
+            return 2;
+        } else {
+            for (List c : conditions) {
+                if (playerPositions.containsAll(c)) {
+                    return 1;
+                } else if (cpuPositions.containsAll(c)) {
+                    return 0;
+                }
+            }
+            return -1;
         }
-        return -1;
     }
+
     private static void printGameBoard(String gameBoard[][]) {
-        for (String [] row: gameBoard) {
+        for (String[] row : gameBoard) {
             for (String str : row) {
                 System.out.print(str);
             }
             System.out.println();
         }
     }
+
     private static void updateGameBoard(String gameBoard[][], int pos, String user) {
         String symbol = user.equals("player") ? " X " : " 0 ";
         switch (pos) {
@@ -130,6 +146,7 @@ public class Main {
                 break;
         }
     }
+
     private static int getPlayerPos() {
         int pos = 0;
         while ((pos < 1 || pos > 9) || checkPos(pos)) {
